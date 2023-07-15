e20fb81d7e93e1816d1066e830fc779363329f68
Khushi Nikam <92162945+KhushiN893@users.noreply.github.com>
2023-07-15 19:35:13 +0530

    Create inorder.java

---
+++ DAY_1/inorder.java
@@ -0,0 +1,39 @@
+import java.util.* ;
+import java.io.*; 
+/*
+    
+    Following is the Binary Tree node structure:
+
+    public class TreeNode {
+        int data;
+        TreeNode left;
+        TreeNode right;
+        TreeNode() {}
+        TreeNode(int val) { this.data = val; }
+        TreeNode(int val, TreeNode left, TreeNode right) {
+           this.data = val;
+            this.left = left;
+            this.right = right;
+        }
+    }
+
+*/
+
+public class Solution {
+    public static List < Integer > getInOrderTraversal(TreeNode root) {
+    	// Write your code here.
+        //inorder left visit right
+      List<Integer> res = new ArrayList<>();
+      Solution obj = new Solution();
+      obj.helper(root, res);
+        return res;
+    }
+
+    public void helper(TreeNode root, List<Integer> res) {
+        if (root != null) {
+            helper(root.left, res);
+            res.add(root.data);
+            helper(root.right, res);
+        }
+    }
+}
