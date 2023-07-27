045575cb71bd361cda9154f8d0b4db67d12981e9
Khushi Nikam <92162945+KhushiN893@users.noreply.github.com>
2023-07-27 09:12:39 +0530

    Create Reverse The Array.java

---
+++ DAY_10/Reverse The Array.java
@@ -0,0 +1,15 @@
+import java.util.ArrayList;
+
+public class Solution {
+    public static void reverseArray(ArrayList<Integer> arr, int m) {
+        int start = m + 1;
+        int end = arr.size() - 1;
+        while (start < end) {
+            int temp = arr.get(start);
+            arr.set(start, arr.get(end));
+            arr.set(end, temp);
+            start++;
+            end--;
+        }
+    }
+}
