e97a5c85026b46ec25e0fda5010a8dceb0218c2c
Khushi Nikam <92162945+KhushiN893@users.noreply.github.com>
2023-07-29 21:18:10 +0530

    Update Switch Case statement.java

--- DAY_11/Switch Case statement.java
+++ DAY_11/Switch Case statement.java
@@ -1,1 +1,17 @@
 
+public class Solution {
+    public static double areaSwitchCase(int ch, double []a) {
+        // Write your code here
+        double res=0;
+        switch(ch){
+            case 1:
+            res=Math.PI*a[0]*a[0];
+            break;
+            case 2:
+            res=a[0]*a[1];
+            break;
+
+        }
+        return res;
+    }
+}
