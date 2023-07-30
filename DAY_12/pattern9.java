1479851f731459e2eec70af47442f2b649198ef6
Khushi Nikam <92162945+KhushiN893@users.noreply.github.com>
2023-07-30 14:37:27 +0530

    Create pattern9.java

---
+++ DAY_12/pattern9.java
@@ -0,0 +1,39 @@
+class Solution {
+
+/*
+    *    
+   * *   
+  * * *  
+ * * * * 
+* * * * *
+* * * * *
+ * * * * 
+  * * *  
+   * *   
+    *  
+*/
+    void printDiamond(int n) {
+        
+        // Your code here
+         for(int i=0;i<n;i++)
+        {
+            for(int j=0;j<n-i-1;j++)
+            System.out.print(" ");
+            for(int j=0;j<=i;j++)
+            System.out.print("* ");
+            for(int j=0;j<n-i-1;j++)
+            System.out.print(" ");
+            System.out.println();
+        }
+         for(int i=n-1;i>=0;i--)
+        {
+            for(int j=0;j<n-i-1;j++)
+            System.out.print(" ");
+            for(int j=0;j<=i;j++)
+            System.out.print("* ");
+            for(int j=0;j<n-i-1;j++)
+            System.out.print(" ");
+            System.out.println();
+        }
+    }
+}
