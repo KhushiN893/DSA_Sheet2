93c1199f9fe7df123a06ec8fc5b8a2794226d8e6
Khushi Nikam <92162945+KhushiN893@users.noreply.github.com>
2023-07-30 14:49:05 +0530

    Create pattern13.java

---
+++ DAY_12/pattern13.java
@@ -0,0 +1,21 @@
+/*
+1
+2 3
+4 5 6
+7 8 9 10
+11 12 13 14 15
+*/
+class Solution {
+
+    void printTriangle(int n) {
+        int printing=1;
+        for(int i=0;i<n;i++)
+        {
+            for(int j=0;j<=i;j++)
+            {
+                System.out.print(printing++ +" ");
+            }
+            System.out.println();
+        }
+    }
+}
