3c5d55e677dd416465a290507d548b2042a3abfe
Khushi Nikam <92162945+KhushiN893@users.noreply.github.com>
2023-07-30 14:42:32 +0530

    Create pattern10.java

---
+++ DAY_12/pattern10.java
@@ -0,0 +1,28 @@
+class Solution {
+/*
+*
+* *
+* * *
+* * * *
+* * * * *
+* * * *
+* * *
+* *
+*
+*/
+    void printTriangle(int n) {
+        // code here
+        for(int i=1;i<=n;i++)
+        {
+            for(int j=1;j<=i;j++)
+            System.out.print("* ");
+            System.out.println();
+        }
+         for(int i=1;i<=n-1;i++)
+        {
+            for(int j=n-1;j>=i;j--)
+            System.out.print("* ");
+            System.out.println();
+        }
+    }
+}
